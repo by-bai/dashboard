0ea9c9fd1a8d63f24edf7d31f786d2059316955b
by-bai <baihe.soh@gmail.com>
2023-02-06 17:08:35 +0800

    added basic recharts charts in lib

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
-
-import Bar from "./lib/Bar";
-import Line from "./lib/Line";
-import Pie from "./lib/Pie";
-import { barData } from "./data/MockData";
-import { lineData } from "./data/MockData";
-import { pieData } from "./data/MockData"; 
+import Bar from './lib/NivoBar';
+import Line from './lib/NivoLine';
+import Pie from './lib/NivoPie';
+import RechartsLine from './lib/RechartsLine';
+import { nivoBarData, nivoLineData, nivoPieData } from './data/NivoData';
+import {
+    rechartsLineData,
+    rechartsBarData,
+    rechartsPieData,
+} from './data/RechartsData';
+import RechartsPie from './lib/RechartsPie';
+import RechartsBar from './lib/RechartsBar';
 
 export default function App() {
-  return (
+    return (
+        <div>
+            <div style={{ height: 500 }}>
+                <Bar data={nivoBarData} />
+            </div>
 
+            <div style={{ height: 500 }}>
+                <Line data={nivoLineData} />
+            </div>
 
- 
-    <div>
+            <div style={{ height: 500 }}>
+                <Pie data={nivoPieData} />
+            </div>
 
-    <div style ={{height: 500}}>
+            <div style={{ height: 500 }}>
+                <RechartsLine data={rechartsLineData} />
+            </div>
 
-      <Bar data={barData}/>
-      
+            <div style={{ height: 500 }}>
+                <RechartsPie data={rechartsPieData} />
+            </div>
 
-    </div>
-
-    <div style={{height: 500}}>
-
-      <Line data={lineData}/>
-    </div>
-   
-    <div style={{height: 500}}>
-
-      <Pie data={pieData}/>
-    </div>
-  
-    
-
-    </div>
-  )
+            <div style={{ height: 500 }}>
+                <RechartsBar data={rechartsBarData} />
+            </div>
+        </div>
+    );
 }
