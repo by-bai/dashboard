0aef7546ae7a7c019fdd8986762da36312b52ba6
by-bai <baihe.soh@gmail.com>
2023-02-08 17:12:34 +0800

    grouped charts

--- src/App.js
+++ src/App.js
@@ -22,37 +22,52 @@ import VLine from './lib/VictoryLine';
 export default function App() {
     return (
         <div>
-            <div style={{ height: 500 }}>
-                <Bar data={nivoBarData} />
-            </div>
-            <div style={{ height: 500 }}>
-                <Line data={nivoLineData} />
-            </div>
-            <div style={{ height: 500 }}>
-                <Pie data={nivoPieData} />
-            </div>
-            <div style={{ height: 500 }}>
-                <RechartsLine data={rechartsLineData} />
-            </div>
-            <div style={{ height: 500 }}>
-                <RechartsPie data={rechartsPieData} />
-            </div>
-            <div style={{ height: 500 }}>
-                <RechartsBar data={rechartsBarData} />
-            </div>
+            <div class='grid grid-cols-3 gap-4'>
+                <h1 class='text-3xl font-extrabold text-purple-600'>Nivo</h1>
+                <div></div>
+                <div></div>
 
-            <div style={{ height: 800 }}>
-                <VBar data={victoryBarData} />
-            </div>
+                <div style={{ height: 500 }}>
+                    <Bar data={nivoBarData} />
+                </div>
+                <div style={{ height: 500 }}>
+                    <Line data={nivoLineData} />
+                </div>
+                <div style={{ height: 500 }}>
+                    <Pie data={nivoPieData} />
+                </div>
 
-            <div style={{ height: 800 }}>
-                {' '}
-                <VPie data={victoryPieData} />
-            </div>
+                <h1 class='text-3xl font-extrabold text-purple-600'>
+                    Recharts
+                </h1>
+                <div></div>
+                <div></div>
 
-            <div style={{ height: 800 }}>
-                {' '}
-                <VLine data={victoryLineData} />
+                <div style={{ height: 500 }}>
+                    <RechartsLine data={rechartsLineData} />
+                </div>
+                <div style={{ height: 500 }}>
+                    <RechartsPie data={rechartsPieData} />
+                </div>
+                <div style={{ height: 500 }}>
+                    <RechartsBar data={rechartsBarData} />
+                </div>
+
+                <h1 class='text-3xl font-extrabold text-purple-600'>Victory</h1>
+                <div></div>
+                <div></div>
+
+                <div style={{ height: 800 }}>
+                    <VBar data={victoryBarData} />
+                </div>
+
+                <div style={{ height: 800 }}>
+                    <VPie data={victoryPieData} />
+                </div>
+
+                <div style={{ height: 800 }}>
+                    <VLine data={victoryLineData} />
+                </div>
             </div>
         </div>
     );
